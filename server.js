a7394a600b44316d02e85c67326ae271b58576e9
piyush-pp <piyush.pande95@gmail.com>
2020-09-12 18:32:37 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express =require('express')
+const app=express()
+const ejs=require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+const PORT=process.env.PORT || 3000
+
+app.get('/', (re,res)=>{
+    res.render('home')
+})
+
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine', 'ejs')
+
+
+app.listen(PORT,(re,res)=>{
+    console.log('server started')
+})
