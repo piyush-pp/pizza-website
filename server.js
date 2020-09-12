a066286a43e32c4a595353a6896dcf7f41838465
piyush-pp <piyush.pande95@gmail.com>
2020-09-13 00:19:48 +0530

    made home page

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const express =require('express')
-const app=express()
-const ejs=require('ejs')
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
-const PORT=process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-app.get('/', (re,res)=>{
+
+app.use(express.static('public'))
+app.get('/', (re, res) => {
     res.render('home')
 })
 
 app.use(expressLayout)
-app.set('views',path.join(__dirname,'/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 
-app.listen(PORT,(re,res)=>{
+app.listen(PORT, (re, res) => {
     console.log('server started')
 })
